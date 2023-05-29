55de45dfb41f740e7b781bb4440f003fe17d85f4
kyrers <kyrers.eth@gmail.com>
2023-05-29 21:40:46 +0100

    Minor improvements to submission queries

--- utils/queries.ts
+++ utils/queries.ts
@@ -1,7 +1,8 @@
 /* USERS */
 export const SELECT_ALL_USERS_QUERY = "SELECT * FROM users";
 export const SELECT_USER_BY_PIN_QUERY = "SELECT * FROM users WHERE pin = $1";
-export const SELECT_USER_BY_TOKEN_QUERY = "SELECT * FROM users WHERE access_token = $1";
+export const SELECT_USER_BY_TOKEN_QUERY =
+  "SELECT * FROM users WHERE access_token = $1";
 export const SELECT_USER_BY_ID_QUERY = "SELECT * FROM users WHERE id = $1";
 export const INSERT_OR_UPDATE_USER_QUERY = `
     INSERT INTO users (pin, discord_id, name, discriminator, code, access_token, refresh_token, expires_in, admin)
@@ -23,8 +24,10 @@ export const INSERT_OR_UPDATE_USER_QUERY = `
 /* LEVELS */
 export const SELECT_ALL_LEVELS_QUERY = "SELECT * FROM levels";
 export const SELECT_LEVEL_BY_ID_QUERY = "SELECT * FROM levels WHERE id = $1";
-export const SELECT_TEST_FILE_BY_ID_QUERY = "SELECT test_contract FROM levels WHERE id = $1";
-export const SELECT_LEVEL_TOTAL_SOLUTIONS = "SELECT COUNT(DISTINCT user_id) FROM submissions WHERE level_id = $1";
+export const SELECT_TEST_FILE_BY_ID_QUERY =
+  "SELECT test_contract FROM levels WHERE id = $1";
+export const SELECT_LEVEL_TOTAL_SOLUTIONS =
+  "SELECT COUNT(DISTINCT user_id) FROM submissions WHERE level_id = $1";
 export const INSERT_LEVEL_QUERY = `
     INSERT INTO levels (name, position, test_contract)
     VALUES($1, $2, $3)
@@ -40,10 +43,14 @@ export const UPDATE_LEVEL_QUERY = `
 export const DELETE_LEVEL_QUERY = "DELETE FROM levels WHERE id = $1";
 
 /* SUBMISSION */
-export const SELECT_ALL_SUBMISSIONS_QUERY = "SELECT * FROM submissions";
-export const SELECT_SUBMISSION_BY_TOKEN_AND_LEVEL_QUERY = "SELECT s.* FROM submissions s INNER JOIN users u ON s.user_id = u.id WHERE u.access_token = $1 AND level_id = $2";
-export const SELECT_SUBMISSION_BY_ID_QUERY = "SELECT * FROM submissions WHERE id = $1";
-export const SELECT_SUBMISSION_BY_BYTECODE_AND_LEVEL_QUERY = "SELECT * FROM submissions WHERE bytecode = $1 AND level_id = $2";
+export const SELECT_ALL_SUBMISSIONS_QUERY =
+  "SELECT s.*, u.name AS user_name, u.discriminator AS discriminator FROM submissions s JOIN users u ON s.user_id = u.id";
+export const SELECT_SUBMISSION_BY_TOKEN_AND_LEVEL_QUERY =
+  "SELECT s.*, u.name AS user_name, u.discriminator AS discriminator FROM submissions s JOIN users u ON s.user_id = u.id WHERE u.access_token = $1 AND level_id = $2";
+export const SELECT_SUBMISSION_BY_ID_QUERY =
+  "SELECT s.*, u.name AS user_name, u.discriminator AS discriminator FROM submissions s JOIN users u ON s.user_id = u.id WHERE id = $1";
+export const SELECT_SUBMISSION_BY_BYTECODE_AND_LEVEL_QUERY =
+  "SELECT s.*, u.name AS user_name, u.discriminator AS discriminator FROM submissions s JOIN users u ON s.user_id = u.id WHERE bytecode = $1 AND level_id = $2";
 export const SELECT_GAS_LEADERBOARD_BY_LEVEL_QUERY = `
     SELECT s.id, s.user_id, s.level_id, s.gas, s.size, s.submitted_at, s.type, s.optimized_for, u.name AS user_name, u.discriminator AS discriminator, l.name AS level_name
     FROM submissions s 
